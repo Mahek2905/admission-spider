2ff3ebf5b2cdd1e7bd2c038e701754bf659d0304
Mahek <152728593+Mahek2905@users.noreply.github.com>
2025-04-11 22:19:44 +0530

    modified few files

--- js-files/script.js
+++ js-files/script.js
@@ -51,17 +51,17 @@ const updateList = (data) => {
 
             const deadline = document.createElement("div");
             deadline.classList.add("deadline");
-            deadline.innerHTML = `<i class="fa-solid fa-calendar"></i> Deadline: <p>${item.application_deadline}</p>`;
+            deadline.innerHTML = `<i class='bx bx-calendar' ></i> Deadline: <p>${item.application_deadline}</p>`;
             clgBox.appendChild(deadline);
 
             const state = document.createElement("div");
             state.classList.add("state");
-            state.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${item.state.name}`;
+            state.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1rem" viewBox="0 -960 960 960" width="1rem" fill="#4b5563"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg> ${item.state.name}`;
             clgBox.appendChild(state);
 
             const announcement = document.createElement("div");
             announcement.classList.add("announcement");
-            announcement.innerHTML = `<i class="fa-solid fa-circle-info"></i> ${item.content}`;
+            announcement.innerHTML = `<i class='bx bx-info-circle' ></i> ${item.content}`;
             clgBox.appendChild(announcement);
 
             const lowerBox = document.createElement("div");
